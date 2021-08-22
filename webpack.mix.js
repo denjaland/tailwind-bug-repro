b41f2f35060c514f34c95138b837b764396fd78d
denjaland <david@denja.land>
2021-08-22 10:49:36 +0200

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+let atImport = require('postcss-import');
+
+
+mix.postCss('src/tailwind.css', 'public/css', [
+        atImport(),
+        tailwindcss('src/tailwind.config.js'),
+    ])
+    .minify('./public/css/tailwind.css')
+    .copy('src/index.html', 'public')
+;
+
+
+
+
+
